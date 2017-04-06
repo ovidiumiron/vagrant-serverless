1b5715217cfc140c25c35db201eca156350f9ab4
Ovidiu Miron <ovidiu.miron@ammeon.com>
2017-04-06 18:29:55 +0100

    install aws

--- vagrant-files/script.sh
+++ vagrant-files/script.sh
@@ -1,6 +1,22 @@
-curl -L https://nodejs.org/dist/v6.10.2/node-v6.10.2-linux-x64.tar.xz -o /tmp/node-v6.10.2-linux-x64.tar.xz &&
+yum -y update
+
+# Install nodejs
+curl -L https://nodejs.org/dist/v6.10.2/node-v6.10.2-linux-x64.tar.xz -o /tmp/node-v6.10.2-linux-x64.tar.xz && 
 tar xf /tmp/node-v6.10.2-linux-x64.tar.xz -C /opt/ &&
-ln -s /opt/node-v6.10.2-linux-x64/bin/node /usr/bin/ &&
-ln -s /opt/node-v6.10.2-linux-x64/bin/npm /usr/bin/npm &&
-npm install -g serverless &&
-ln -s /opt/node-v6.10.2-linux-x64/lib/node_modules/serverless/bin/serverless /usr/sbin/serverless
+ln -sf /opt/node-v6.10.2-linux-x64/bin/node /usr/bin/ &&
+ln -sf /opt/node-v6.10.2-linux-x64/bin/npm /usr/bin/npm &&
+# Install serverless
+npm install -g serverless && #dfsd
+ln -sf /opt/node-v6.10.2-linux-x64/bin/serverless /usr/sbin/serverless &&
+ln -sf /opt/node-v6.10.2-linux-x64/bin/slss /usr/sbin/serverless &&
+
+#Install utilities
+yum install -y tree &&
+yum install -y vim &&
+
+# Install aws
+yum --enablerepo=extras install epel-release -y &&
+yum install -y python-pip &&
+pip install --upgrade pip &&
+yum install  gcc libffi-devel python-devel openssl-devel -y &&
+pip install aws 
